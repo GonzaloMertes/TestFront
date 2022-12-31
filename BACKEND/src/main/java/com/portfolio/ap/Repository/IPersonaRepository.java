f26030a73f301445daee1c60fe4455901d27609b
GonzaloMertes <gonzaaameeerteees@gmail.com>
2022-12-31 10:05:35 -0300

    se dejo solo front

--- BACKEND/src/main/java/com/portfolio/ap/Repository/IPersonaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.portfolio.ap.Repository;
-
-import com.portfolio.ap.entity.Persona;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IPersonaRepository extends JpaRepository <Persona,Long>{  
-}
