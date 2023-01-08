3230819e635d3f19612025e5c7c89492d2c6250a
GonzaloMertes <gonzaaameeerteees@gmail.com>
2023-01-08 10:07:42 -0300

    terminando el edit hys

--- src/app/components/hys/edit-skill.component.ts
+++ src/app/components/hys/edit-skill.component.ts
@@ -4,6 +4,7 @@ import { Skill } from 'src/app/model/skill';
 import { ImageSService } from 'src/app/service/image-s.service';
 import { ImageService } from 'src/app/service/image.service';
 import { SkillService } from 'src/app/service/skill.service';
+import { ref,Storage,uploadBytes, getDownloadURL, getStorage, deleteObject } from '@angular/fire/storage';
 
 @Component({
   selector: 'app-edit-skill',
@@ -12,11 +13,13 @@ import { SkillService } from 'src/app/service/skill.service';
 })
 export class EditSkillComponent implements OnInit {
   skill: Skill = null;
+  img:string;
 
   constructor(private activatedRouter:ActivatedRoute, 
     private Sskill:SkillService, 
     private router: Router,
-    public imageService: ImageSService) { }
+    public imageService: ImageSService,
+    public storage:Storage) { }
 
     ngOnInit(): void {
       const id = this.activatedRouter.snapshot.params['id'];
@@ -31,8 +34,9 @@ export class EditSkillComponent implements OnInit {
     }
   
     onUpdate():void{
+      
       const id = this.activatedRouter.snapshot.params['id'];
-      this.skill.img = this.imageService.url;
+      
       this.Sskill.update(id, this.skill).subscribe(
         data => {
           this.router.navigate(['']);
@@ -45,8 +49,47 @@ export class EditSkillComponent implements OnInit {
 
     //debemos colocar una forma de elimnar la foto del storage cuando se elimina un hys
     
-  
+    public uploadImage( $event:any){
+      const id = this.activatedRouter.snapshot.params['id'];
+      this.delete(id)
+      const file = $event.target.files [0];
+      
+      //colocamos el lugar adonde se descargara la imagen en el storage 
+      const ImagesRef = ref(this.storage, `imageSkill/${file.name}`);
+        
+      uploadBytes(ImagesRef, file)
+      .then((snapshot) => {
+        console.log('se subio el archivo');
+      })
+      .catch(error => console.log(error))
+      .then( download =>{
     
+        const storage = getStorage();
+      getDownloadURL(ref(storage, `imageSkill/${file.name}` ))
+      .then((url) => {
+        
+        // `url` is the download URL for 'imageSkill/'+name
+    
+        this.skill.img = url;
+       
+    
+      })
+      .catch((error) => { console.log(error);
+      });
+    
+      }); 
+      
+    
+    }
+    
+    delete(id: number){
+  const desertRef = ref(this.storage, `${this.skill.img}`);
+  deleteObject (desertRef).then(() => {
+    console.log("Se pudo elimnar la imagen")
+  }).catch((error) => {
+    console.log("No se pudo elimnar la imagen")
+  });
+}
 
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,6 +14,7 @@ export const environment = {
   },
   production: false,
   URL:'http://localhost:8080/'
+  //URL:'https://textback-production.up.railway.app/'
   
 };
 
